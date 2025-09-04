e7ca7b2f66c6b05100bf24c13a3cb4694a7c9c4e
LAPTOP-CRA5A6GT\shnak <oolongtea1929@gmail.com>
2025-09-05 06:31:36 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
 
